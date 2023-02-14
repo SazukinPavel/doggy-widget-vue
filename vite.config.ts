0691ede508d146b7fe47a5ac1ab033b1383d281b
SazukinPavel <sazukinpavelal@gmail.com>
2023-02-14 19:02:26 +0300

    fix all

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "dist/plugin/",
+        entryFileNames: "plugin.js",
+        assetFileNames: "plugin.css",
+        chunkFileNames: "chunk.js",
+        manualChunks: undefined,
+      },
+    },
+  },
+});
