78ab782ed52a955ed644483818297e4b22ca8b86
SazukinPavel <sazukinpavelal@gmail.com>
2023-02-15 10:55:40 +0300

    add css in js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [cssInjectedByJsPlugin(), vue()],
   build: {
     rollupOptions: {
       output: {
